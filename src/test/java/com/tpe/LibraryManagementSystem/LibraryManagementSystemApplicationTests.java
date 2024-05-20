e2e5ce6c1b3f9dd4640e999ee2466f4dbf968257
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-20 20:01:37 +0300

    Library Management System Practice Project from SpingBoot is created

---
+++ src/test/java/com/tpe/LibraryManagementSystem/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpe.LibraryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
