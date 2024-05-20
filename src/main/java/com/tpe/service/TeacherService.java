4f722579ef7f031c6cf63d903febfb7582632d5c
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-20 21:27:24 +0300

    Library Management System Practice Project, packages/classes

--- src/main/java/com/tpe/service/TeacherService.java
+++ src/main/java/com/tpe/service/TeacherService.java
@@ -3,7 +3,9 @@ package com.tpe.service;
 
 import com.tpe.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TeacherService {
 
     /*
