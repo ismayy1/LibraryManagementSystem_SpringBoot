f4e8ac0d9bab79d0a9cec7eaaa4619a47639da37
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-20 21:11:23 +0300

    Library Management System Practice Project, packages/classes

--- src/main/java/com/tpe/controller/TeacherController.java
+++ src/main/java/com/tpe/controller/TeacherController.java
@@ -1,7 +1,13 @@
 package com.tpe.controller;
 
+import com.tpe.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TeacherController {
+
+    @Autowired
+    private TeacherService service;
+
 }
--- src/main/java/com/tpe/service/TeacherService.java
+++ src/main/java/com/tpe/service/TeacherService.java
@@ -1,5 +1,18 @@
 package com.tpe.service;
 
 
+import com.tpe.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class TeacherService {
+
+    /*
+        we have 3 types of injections in springBoot:
+            1. Failed injection
+            2. Constructor injection
+            3. Setter injection
+     */
+    @Autowired  // this injection called Failed injection
+    private TeacherRepository teacherRepository;
+
 }
